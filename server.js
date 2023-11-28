d94aa7fc5d17696a4c628b00fcbe425824bc1cca
cam8anderson <cam8anderson@icloud.com>
2023-11-28 16:48:49 -0700

    :construction: adding rollbar

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
-rollbar.log("Hello cat");
+rollbar.log("Hello cats button");
 
 app.use(express.static(`${__dirname}/public`));
 
