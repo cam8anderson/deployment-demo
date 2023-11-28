2484f7058ed371e2725f71b4eb9f97f984fefe92
cam8anderson <cam8anderson@icloud.com>
2023-11-28 16:31:08 -0700

    :construction: adding rollbar

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
+const Rollbar = require("rollbar");
+
 const btn = document.querySelector("button");
 
-const clickHandler = () => alert("The cat name is: Rascal");
+const clickHandler = () => rollbar.log("cat button on");
+alert("The cat name is: Rascal");
 
 btn.addEventListener("click", clickHandler);
